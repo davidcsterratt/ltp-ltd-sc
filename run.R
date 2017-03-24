b38e2afa8fdea2bb338a5e99e2e4bacaeee508cf
David C Sterratt <david.c.sterratt@ed.ac.uk>
2017-03-24 15:01:29 +0000

    Plot the Carlisle & al Fig 3 WT time series graphs

--- run.R
+++ run.R
@@ -8,11 +8,11 @@ read.carletal <- function(file="fig3a_wt.csv", dir="../../data/CarlEtal08oppo/")
                   ))
 }
 
-plot.ts <- function(dat, ylim=c(0, 200), add=FALSE, hue=1, alpha=0.5) {
+plot.ts <- function(dat, ylim=c(0, 200), add=FALSE, hue=1, alpha=0.5, ...) {
   if (!add) {
     plot(NA, NA, 
          xlim=range(dat$time), ylim=ylim,
-         xlab="Time (minutes)", ylab="Strength %")
+         xlab="Time (minutes)", ylab="Strength (% Baseline)", ...)
   }
   n <- length(dat$time)
   polygon(c(dat$time, dat$time[n:1]),
@@ -24,21 +24,40 @@ plot.ts <- function(dat, ylim=c(0, 200), add=FALSE, hue=1, alpha=0.5) {
   # lines(dat$time, apply(select(dat, -time), 1, min), type='l', ylim=ylim)
 }
 
-sims <- lapply(1:20, function(x) {run.kasim("maguk.ka", l=67*60, p=10)})
-par(mfrow=c(2, 1),
-    mar=c(2.4, 3, 1.5, 0.5),
-    mgp=c(1.3, 0.4, 0))
-plot.kasim(sims[[1]])
-
-dat <- read.carletal("fig3a_wt.csv")
-
-#matplot(select(dat, time), select(dat, -time), pch=19, ylim=c(0, 200))
-
-
-stg.psd95.sim <- data.frame(time=sims[[1]][,"[T]"]/60 - 20,
+run.kasims <- function(files="maguk.ka", l=67*60, p=10, n=20) {
+  lapply(1:n, function(x) {run.kasim(files=files, l=l, p=p)})
+  stg.psd95 <- data.frame(time=sims[[1]][,"[T]"]/60 - 20,
                        do.call(cbind,
                                lapply(sims,
                                       function(x) {return(x[,"stargazin-PSD95"])}))/10*100)
+  return(list(raw=raw, stg.psd95=stg.psd95))
+}
+                       
+## Plot simulations
+par(mfcol=c(2, 2),
+    mar=c(2.4, 3, 1.5, 0.5),
+    mgp=c(1.3, 0.4, 0))
 
-plot.ts(dat)
-plot.ts(stg.psd95.sim, add=TRUE, hue=0.5)
+## Fig A - 5Hz/5 sec
+plot.ts(read.carletal("fig3a_wt.csv"), main="A: 5Hz/5 sec")
+sims.a <- run.kasims("maguk.ka", l=67*60, p=10)
+plot.ts(sims.a$stg.psd95, add=TRUE, hue=0.5)
+
+## Fig B - 5Hz/1 min
+plot.ts(read.carletal("fig3b_wt.csv"), main="B: 5Hz/15 sec")
+sims.b <- run.kasims("maguk.ka", l=67*60, p=10)
+plot.ts(sims.b$stg.psd95, add=TRUE, hue=0.5)
+
+## Fig C - 5Hz/15 second
+plot.ts(read.carletal("fig3c_wt.csv"), main="C: 5Hz/1 min")
+sims.c <- run.kasims("maguk.ka", l=67*60, p=10)
+plot.ts(sims.c$stg.psd95, add=TRUE, hue=0.5)
+
+#matplot(select(dat, time), select(dat, -time), pch=19, ylim=c(0, 200))
+
+# plot.kasim(sims[[1]])
+
+
+
+
+# plot.ts(stg.psd95.sim, add=TRUE, hue=0.5)
